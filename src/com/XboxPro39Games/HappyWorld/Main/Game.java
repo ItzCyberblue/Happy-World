7123c4938c36473e8e492feffbfc246f693bcbc5
XboxPro39 <57122545+XboxPro39@users.noreply.github.com>
2019-12-11 20:37:25 -0600

    Update Game.java

--- src/com/XboxPro39Games/HappyWorld/Main/Game.java
+++ src/com/XboxPro39Games/HappyWorld/Main/Game.java
@@ -1,8 +1,13 @@
 package com.XboxPro39Games.HappyWorld.Main;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.BorderLayout;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
@@ -17,6 +22,10 @@ public class Game extends Canvas implements Runnable{
 	private JFrame frame;
 	
 	private boolean running = false;
+	public int tickCount = 0;
+	
+	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
 	
 	public Game() {
 		setMinimumSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
@@ -46,31 +55,69 @@ public class Game extends Canvas implements Runnable{
 	}
 
 	public void run() {
-		long lasTime = System.nanoTime();
+		long lastTime = System.nanoTime();
 		double nsPerTick = 1000000000D/60D;
 		
 		int ticks = 0;
 		int frames = 0;
 		
-		long lasTimer = System.currentTimeMillis();
+		long lastTimer = System.currentTimeMillis();
 		double delta = 0;
 		
 		while(running) {
 			long now = System.nanoTime();
-			delta += (now - lasTime) / nsPerTick;
-			lasTime = now;
+			delta += (now - lastTime) / nsPerTick;
+			lastTime = now;
+			boolean shouldRender = false;
 			
-			tick();
-			render();
+			while(delta >= 1) {
+				ticks++;
+				tick();
+				delta -= 1;
+				shouldRender = true;
+			}
+			
+			if(shouldRender) {
+				frames++;
+				render();
+			}
+			
+			if(System.currentTimeMillis() - lastTimer >= 1000) {
+				lastTimer += 1000;
+				System.out.println(ticks + " Ticks, " + frames + " FPS");
+				frames = 0;
+				ticks = 0;
+			}
 		}
 	}
 	
 	public void tick() {
+		tickCount++;
 		
+		for(int i = 0; i < pixels.length; i++) {
+			pixels[i] = i + tickCount;
+		}
 	}
 	
 	public void render() {
+		BufferStrategy bs = getBufferStrategy();
 		
+		if(bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+		
+		g.setColor(Color.BLACK);
+		g.drawRect(0, 0, getWidth(), getHeight());
+		g.dispose();
+		bs.show();
 	}
 	
 	public static void main(String[] args) {
